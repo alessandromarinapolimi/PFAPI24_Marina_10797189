d5ce703b38080431e24769fd1cab2d31def0eb90
alessandromarinapolimi <alessandro.marina@mail.polimi.it>
2024-06-03 13:46:05 +0200

    fixed add_ingredient, add_ingredient_to_recipe to be fixed next

--- Open.c
+++ Open.c
@@ -32,7 +32,9 @@ Ingredient *ingredients_total = NULL; // Dynamic array of total ingredients
 typedef struct
 {
     char name[WORD_SIZE];
-    Ingredient *ingredients;
+    char **ingredient_names;
+    unsigned int **ingredient_quantities;
+    unsigned int *needed_quantities;
     unsigned int num_ingredients;
 } Recipe;
 
@@ -126,18 +128,18 @@ void add_ingredient(char *name, unsigned int quantity, int expiration_time)
 void init_recipe(Recipe *recipe, char *name, unsigned int num_ingredients)
 {
     strcpy(recipe->name, name);
-    recipe->ingredients = malloc(num_ingredients * sizeof(Ingredient));
+    recipe->ingredient_names = malloc(num_ingredients * sizeof(char *));
+    recipe->ingredient_quantities = malloc(num_ingredients * sizeof(unsigned int *));
+    recipe->needed_quantities = malloc(num_ingredients * sizeof(unsigned int));
     recipe->num_ingredients = num_ingredients;
 }
 
 // O(n) time, O(1) space
-void add_ingredient_to_recipe(Ingredient *recipe_ingredient, char *name, unsigned int quantity)
+void add_ingredient_to_recipe(Recipe *recipe, unsigned int ingredient_index, char *name, unsigned int *quantity, unsigned int needed_quantity)
 {
-    strcpy(recipe_ingredient->name, name);
-    recipe_ingredient->total_quantity = quantity;
-    recipe_ingredient->batches = NULL; // No batches for ingredients in recipes
-    recipe_ingredient->num_batches = 0;
-    recipe_ingredient->max_batches = 0;
+    recipe->ingredient_names[ingredient_index] = name;
+    recipe->ingredient_quantities[ingredient_index] = quantity;
+    recipe->needed_quantities[ingredient_index] = needed_quantity;
 }
 
 // O(n) time, O(n) space (where n is the number of ingredients in the recipe)
@@ -164,8 +166,20 @@ void aggiungi_ricetta(char *name, char **ingredients, unsigned int *quantities, 
     init_recipe(&recipes[num_recipes], name, num_ingredients);
     for (unsigned int i = 0; i < num_ingredients; i++)
     {
-        add_ingredient(ingredients[i], quantities[i], -1); // Add ingredient to the global list without specifying expiration time
-        add_ingredient_to_recipe(&recipes[num_recipes].ingredients[i], ingredients[i], quantities[i]);
+        // Add ingredient to the global list without specifying expiration time
+        add_ingredient(ingredients[i], 0, -1);
+        
+        // Find the ingredient in the global list
+        unsigned int j;
+        for (j = 0; j < num_ingredients_total; j++)
+        {
+            if (strcmp(ingredients_total[j].name, ingredients[i]) == 0)
+            {
+                break;
+            }
+        }
+        
+        add_ingredient_to_recipe(&recipes[num_recipes], i, ingredients_total[j].name, &ingredients_total[j].total_quantity, quantities[i]);
     }
 
     num_recipes++;
@@ -175,43 +189,56 @@ void aggiungi_ricetta(char *name, char **ingredients, unsigned int *quantities, 
 // O(n) time, O(n) space (where n is the length of the input line)
 void manage_aggiungi_ricetta(char *line)
 {
-    // Extract the recipe name from the input line
-    char name[WORD_SIZE];
-    char *token = strtok(line + 2, " "); // Skip the command "aggiungi_ricetta"
-    token = strtok(NULL, " ");           // Extract the recipe name
-    strcpy(name, token);
+    // Skip the command "aggiungi_ricetta"
+    char *token = strtok(line + strlen("aggiungi_ricetta") + 1, " ");
+    // Extract the recipe name
+    char *name = token;
 
-    // Extract the maximum number of ingredients from the input line
-    token = strtok(NULL, " ");
-    unsigned int max_ingredients = atoi(token);
-
-    // Dynamically allocate the ingredients array
-    char **ingredients = malloc(max_ingredients * sizeof(char *));
-
-    // Dynamically allocate the quantities array
-    unsigned int *quantities = malloc(max_ingredients * sizeof(unsigned int));
-
-    // Count the number of ingredients
+    // Initialize variables to store ingredient information
+    char **ingredients = NULL;
+    unsigned int *quantities = NULL;
+    unsigned int max_ingredients = 0;
     unsigned int num_ingredients = 0;
-    token = strtok(NULL, " ");
-    while (token != NULL && num_ingredients < max_ingredients)
+
+    // Parse the rest of the string to extract ingredient names and quantities
+    while ((token = strtok(NULL, " ")) != NULL)
     {
-        ingredients[num_ingredients] = strdup(token);
+        // Check if we have enough space in the arrays, if not, resize them
+        if (num_ingredients >= max_ingredients)
+        {
+            max_ingredients += 1;
+            ingredients = realloc(ingredients, max_ingredients * sizeof(char *));
+            quantities = realloc(quantities, max_ingredients * sizeof(unsigned int));
+        }
+
+        // Extract ingredient name and quantity
+        char *ingredient_name = token;
         token = strtok(NULL, " ");
-        quantities[num_ingredients] = atoi(token);
-        token = strtok(NULL, " ");
+        unsigned int quantity = atoi(token);
+
+        // Add the ingredient to the arrays
+        ingredients[num_ingredients] = ingredient_name;
+        quantities[num_ingredients] = quantity;
         num_ingredients++;
     }
 
     // Call the aggiungi_ricetta function
+    printf("Chiamata aggiungi_ricetta con i seguenti parametri:\n");
+    printf("Nome ricetta: %s\n", name);
+    printf("Ingredienti:\n");
+    for (unsigned int i = 0; i < num_ingredients; i++)
+    {
+        printf("Ingrediente %u: %s, Quantità: %u\n", i + 1, ingredients[i], quantities[i]);
+    }
     aggiungi_ricetta(name, ingredients, quantities, num_ingredients);
 
     // Free the memory allocated for the ingredients
-    for (unsigned int i = 0; i < num_ingredients; i++)
-        free(ingredients[i]);
     free(ingredients);
     free(quantities);
 }
+
+
+
 
 // O(n) time, O(1) space
 void print_ingredient_table()
@@ -235,6 +262,7 @@ void print_ingredient_table()
         }
     }
 }
+
 // O(n) time, O(1) space
 void print_recipe_table()
 {
@@ -244,7 +272,7 @@ void print_recipe_table()
         printf("Recipe: %s, Ingredients:\n", recipes[i].name);
         for (unsigned int j = 0; j < recipes[i].num_ingredients; j++)
         {
-            printf("  Ingredient: %s, Quantity: %u\n", recipes[i].ingredients[j].name, recipes[i].ingredients[j].total_quantity);
+            printf("  Ingredient: %s, Quantity: %u\n", recipes[i].ingredient_names[j], recipes[i].needed_quantities[j]);
         }
     }
 }
@@ -319,7 +347,11 @@ int main()
         free(ingredients_total[i].batches);
     free(ingredients_total);
     for (unsigned int i = 0; i < num_recipes; i++)
-        free(recipes[i].ingredients);
+    {
+        free(recipes[i].ingredient_names);
+        free(recipes[i].ingredient_quantities);
+        free(recipes[i].needed_quantities);
+    }
     free(recipes);
 
     return 0;
